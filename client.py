1f74328b7573893dba6ff472e2af3ba708f74968
AllanCodes <aeivazia@uci.edu>
2017-08-05 13:56:35 -0700

    working on threading

--- client.py
+++ client.py
@@ -2,20 +2,27 @@ import socket
 
 
 class client():
-    """docstring for client."""
+    """Client"""
     def __init__(self):
         self.sock = None
 
     def create_socket(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def connect(self, host=socket.gethostname(), port=8880):
+    def connect(self, host=socket.gethostname(), port=8980):
         if host is None:
             self.sock.connect((host, port))
         else:
             self.sock.connect((host, port))
 
+    def send(self):
+        while True:
+            msg = input("you: ")
+            self.sock.sendto(msg.encode(), (socket.gethostname(), 8980))
+            print(self.sock.recv(1024).decode())
+
 
 ok = client()
 ok.create_socket()
 ok.connect()
+ok.send()
--- server.py
+++ server.py
@@ -1,33 +1,48 @@
 import socket
 import threading
+import select
+
 
 class server():
-    """docstring for server."""
+    """threaded server"""
     def __init__(self):
         self.serversocket = None
+        self.clientsocket = None
+        self.threads = []
+
+    def get_message(self, clientsocket):
+        while True:
+            data = clientsocket.recv(1024).decode()
+            clientsocket.sendto(data.encode(), (socket.gethostname(), 8980))
+            # data = self.clientsocket.recv(1024).decode()
+            # self.clientsocket.sendto(data.encode(), (socket.gethostname(), 8980))
 
     def create_socket(self):
         try:
             self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         except socket.error:
+            print("socket error: create")
             self.serversocket.close()
 
-    def bind_socket(self, host=socket.gethostname(), port=8880):
+    def bind_socket(self, host=socket.gethostname(), port=8980):
         try:
             self.serversocket.bind((host, port))
         except socket.error:
+            print("socket error: bind")
             self.serversocket.close()
 
     def listen(self, max=5):
         try:
             self.serversocket.listen(max)
         except socket.error:
+            print("socket error: listen")
             self.serversocket.close()
 
     def accept(self):
-        while True:
-            connection = self.serversocket.accept()
-            print(connection)
+        (self.clientsocket, connection) = self.serversocket.accept()
+        print("{} has connected".format(connection))
+        t1 = threading.Thread(target=self.get_message, kwargs={'clientsocket': self.clientsocket})
+        t1.run()
 
 
 ok = server()
